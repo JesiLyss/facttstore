4b94c372e7ca1e9105e026422044ba1d385decd2
JesiLyss <antonella.jesica.gomez@gmail.com>
2024-08-11 18:06:41 -0300

    fixed

---
+++ script.js
@@ -0,0 +1,8 @@
+//hamburger menu
+
+hamburger=document.querySelector(".hamburger");
+hamburger.onclick=function() {
+    navBar=document.querySelector('nav-bar');
+    navBar.classList.toggle('active');
+    
+}
